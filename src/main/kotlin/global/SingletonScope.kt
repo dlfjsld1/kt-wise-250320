cfad7f32b60378166343df5cf587646429804341
dlfjsld1 <ltk900216@gmail.com>
2025-03-21 14:15:00 +0900

    by lazy로 요청시 불러오도록 수정

--- src/main/kotlin/global/SingletonScope.kt
+++ src/main/kotlin/global/SingletonScope.kt
@@ -6,9 +6,10 @@ import com.think.domain.wiseSaying.repository.WiseSayingRepository
 import com.think.domain.wiseSaying.service.WiseSayingService
 
 // 불필요한 객체를 생성하지 않고 일관되게 사용하기 위해 Singleton Scope 사용
+// by lazy{}로 요청 시 불러오도록 함
 object SingletonScope {
-    val wiseSayingRepository = WiseSayingRepository()
-    val wiseSayingService = WiseSayingService()
-    val wiseSayingController = WiseSayingController()
-    val systemController = SystemController()
+    val wiseSayingRepository by lazy { WiseSayingRepository()}
+val wiseSayingService by lazy { WiseSayingService() }
+    val wiseSayingController by lazy { WiseSayingController() }
+    val systemController by lazy { SystemController() }
 }
