950996b2c0cb88c6c1434e6377d664cdb946ad4d
dlfjsld1 <ltk900216@gmail.com>
2025-03-21 12:38:04 +0900

    테스트 목록, 삭제, 수정 추가

--- src/test/kotlin/TestBot.kt
+++ src/test/kotlin/TestBot.kt
@@ -14,23 +14,26 @@ class TestBot {
                 .trimIndent()
                 .plus("\n종료")
 
-            // 결과 값을 저장할 ByteArrayOutputStream 객체를 생성합니다.
-            val out = ByteArrayOutputStream()
+            // use는 사용한 주체가 자동으로 해제되는 것을 보장함
+            // use 안에서 진행되는 람다는 예외가 발생 시 use를 사용한 주체가 자동으로 해제됨
+            // 물론 정상적으로 끝나도 자동으로 해제된다.
+            ByteArrayOutputStream().use { out ->
+                PrintStream(out).use {
+                    try {
+                        System.setOut(it)
+                        System.setIn(formattedInput.byteInputStream()) // 커스텀 입력 - 매개변수 문자열
 
-            val testOut = PrintStream(out) // 커스텀 출력 - 배열
+                        val app = App()
+                        app.run()
+                    } finally {
+                        System.setIn(originalIn) // 표준 입력으로 되돌림
+                        System.setOut(originalOut) // 표준 출력으로 되돌림
+                    }
+                } // 커스텀 출력 - 배열
+                return out.toString().trim().replace("\\r\\n", "\n")
 
-            System.setIn(formattedInput.byteInputStream()) // 커스텀 입력 - 매개변수 문자열
+            } // 결과 값을 저장할 ByteArrayOutputStream 객체를 생성합니다.
 
-            // System.out을 테스트용 OutputStream으로 바꿉니다.
-            System.setOut(testOut)
-
-            val app = App()
-            app.run()
-
-            System.setIn(originalIn) // 표준 입력으로 되돌림
-            System.setOut(originalOut) // 표준 출력으로 되돌림
-
-            return out.toString().trim().replace("\\r\\n", "\n")
 
 
         }
