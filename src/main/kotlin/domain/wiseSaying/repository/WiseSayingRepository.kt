a479cbef647b2ddb7f21e2346a8f4436dd162bf6
dlfjsld1 <ltk900216@gmail.com>
2025-03-21 11:34:42 +0900

    repository 추가

---
+++ src/main/kotlin/domain/wiseSaying/repository/WiseSayingRepository.kt
@@ -0,0 +1,36 @@
+package com.think.domain.wiseSaying.repository
+
+import com.think.domain.wiseSaying.entity.WiseSaying
+
+class WiseSayingRepository {
+    var lastId: Int = 0
+    val wiseSayings = mutableListOf<WiseSaying>()
+
+    fun save(wiseSaying: WiseSaying): WiseSaying {
+
+        if(wiseSaying.isNew()) {
+            val new = wiseSaying.copy(id = ++lastId)
+            wiseSayings.add(new)
+            return new
+        }
+
+        //id가 동일한 첫 번째 인덱스를 찾는다
+        wiseSayings.indexOfFirst { it.id == wiseSaying.id }.let {
+            wiseSayings[it] = wiseSaying
+        }
+        return wiseSaying
+    }
+
+    fun findAll(): List<WiseSaying> {
+        //불변으로 변경해 조회되도록 수정
+        return wiseSayings.toList()
+    }
+
+    fun delete(wiseSaying: WiseSaying) {
+        wiseSayings.remove(wiseSaying)
+    }
+
+    fun findById(id: Int): WiseSaying? {
+        return wiseSayings.find { it.id == id }
+    }
+}
--- src/main/kotlin/domain/wiseSaying/service/WiseSayingService.kt
+++ src/main/kotlin/domain/wiseSaying/service/WiseSayingService.kt
@@ -1,43 +1,32 @@
 package com.think.domain.wiseSaying.service
 
 import com.think.domain.wiseSaying.entity.WiseSaying
+import com.think.domain.wiseSaying.repository.WiseSayingRepository
 
 class WiseSayingService {
-    var lastId: Int = 0
-    val wiseSayings = mutableListOf<WiseSaying>()
+
+    private val wiseSayingRepository = WiseSayingRepository()
 
     fun write(saying: String, author: String): WiseSaying {
-        val id = ++lastId
-        val new = WiseSaying(id, saying, author)
-
-        wiseSayings.add(new)
-
-        return new
+        return wiseSayingRepository.save(WiseSaying(saying = saying, author = author))
     }
 
     fun getitems(): List<WiseSaying> {
-        //불변으로 변경해 조회되도록 수정
-        return wiseSayings.toList()
+        return wiseSayingRepository.findAll()
+
     }
 
     fun delete(wiseSaying: WiseSaying) {
-        val rst = wiseSayings.remove(wiseSaying)
+        wiseSayingRepository.delete(wiseSaying)
     }
 
     fun getItem(id: Int): WiseSaying? {
-        return wiseSayings.find { it.id == id }
+        return wiseSayingRepository.findById(id)
     }
 
     fun modify(wiseSaying: WiseSaying, saying: String, author: String): WiseSaying? {
 
-        //id가 동일한 첫 번째 인덱스를 찾는다
-        val index = wiseSayings.indexOfFirst { it.id == wiseSaying.id }
-
-        //복사본 일부만 커스터마이징해서 만들기
-        val new = wiseSaying.copy(author = author, saying = saying)
-        wiseSayings[index] = new
-
-        return new
+        return wiseSayingRepository.save(wiseSaying.copy(saying = saying, author = author))
 
     }
 }
